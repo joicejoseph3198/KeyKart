6c16ec9bca183d7fd09dfd1531ef8c1cd5d0cb19
Joice V Joseph <joice.joseph3198@gmail.com>
2023-09-16 22:13:10 +0530

    Initial project setup. Configured discovery-service, config-service and API gateway

---
+++ config-service/src/main/java/org/example/ConfigServiceApplication.java
@@ -0,0 +1,13 @@
+package org.example;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+@SpringBootApplication
+@EnableConfigServer
+public class ConfigServiceApplication {
+    public static void main(String[] args) {
+
+
+    }
+}
